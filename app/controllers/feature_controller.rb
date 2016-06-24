c652b84c19aac306e18c8d71341f4ef4ee2fc886
gabrielqueiroz <gabriel.fatec@hotmail.com>
2016-06-24 16:04:29 -0300

    drying and save button

--- app/controllers/feature_controller.rb
+++ app/controllers/feature_controller.rb
@@ -6,30 +6,26 @@ class FeatureController < ApplicationController
   def show
     @feature = params[:id]
 
-    @redis = redis_connection
-    @percentage = @redis.get("feature:#{@feature}:percentage")
-    @users = @redis.smembers("feature:#{@feature}:users")
+    @percentage = redis_connection.get("feature:#{@feature}:percentage")
+    @users = redis_connection.smembers("feature:#{@feature}:users")
   end
 
   def create
-    @redis = redis_connection
-    @redis.set("feature:#{params[:feature]}:percentage", params[:percentage])
-    @redis.sadd("feature:#{params[:feature]}:users", params[:users]) if params[:users]
+    redis_connection.set("feature:#{params[:feature]}:percentage", params[:percentage])
+    redis_connection.sadd("feature:#{params[:feature]}:users", params[:users]) if params[:users]
 
     redirect_to controller: :dashboard, action: :index
   end
 
   def destroy
-    @redis = redis_connection
-    @redis.del("feature:#{params[:id]}:percentage")
-    @redis.del("feature:#{params[:id]}:users")
+    redis_connection.del("feature:#{params[:id]}:percentage")
+    redis_connection.del("feature:#{params[:id]}:users")
 
     render json: { feature: params[:id] }, status: 200
   end
 
   def add_user
     redis_connection.sadd("feature:#{params[:id]}:users", params[:user])
-
     render json: {user: params[:user]}, status: 200
   end
 
