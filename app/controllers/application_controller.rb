be8575be872355601c27676b5b0a536cc4094249
gabrielqueiroz <gabriel.fatec@hotmail.com>
2016-06-24 15:51:21 -0300

    fixing loggin filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :redis_connection
+  before_filter :connection_available?
 
   def redis_connection
-    if @redis
-      @redis = Redis.new(:url => "redis://:#{session[:password]}@#{session[:user]}")
-    else
+    @redis ||= Redis.new(url: "redis://:#{session[:password]}@#{session[:user]}")
+  rescue
+  end
+
+  def connection_available?
+    unless redis_connection
       flash[:error] = "Please, connect to redis"
       redirect_to controller: :login, action: :index
     end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,7 @@
 class LoginController < ApplicationController
 
   before_filter :redis_connection, except: [:index, :create]
+  before_filter :connection_available?, except: [:index, :create]
 
   def index
   end
