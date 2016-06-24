537f63c52b155fdc72a14e9c90e8d11be0140545
André Jardim Protti <andrejardimprotti@gmail.com>
2016-06-24 11:12:55 -0300

    Regex to list features on dashboard page.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,13 @@ class DashboardController < ApplicationController
 
   def index
     @redis = redis_connection
+
+    @features = []
+
+    @redis.keys("*feature*").each do |feature|
+      @features << /feature:(.+):.*/.match(feature)[1]
+    end
+
   end
 
 end
