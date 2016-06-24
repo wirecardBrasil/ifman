e1cf4b555c71e511591593eeb87c96adb7e4a9df
gabrielqueiroz <gabriel.fatec@hotmail.com>
2016-06-24 11:32:16 -0300

    creating and deleting feature

--- app/controllers/feature_controller.rb
+++ app/controllers/feature_controller.rb
@@ -4,12 +4,15 @@ class FeatureController < ApplicationController
 
   end
 
-  def create
-  
+  def create feature
+    @redis = redis_connection
+    @redis.set("feature:#{feature}:percentage", 0)
   end
 
-  def delete
-
+  def delete feature
+    @redis = redis_connection
+    @redis.del("feature:#{feature}:percentage")
+    @redis.del("feature:#{feature}:users")
   end
 
 end
